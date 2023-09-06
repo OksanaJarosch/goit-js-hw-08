56183919694bd225686479074f4901b520c79327
Oksana Jarosch <ok.jarosch@gmail.com>
2023-09-06 23:06:27 +0200

    task1-completet

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"; 
+
+const listEl = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(({description, preview, original}) => {
+    return `<li class="gallery__item" data-name="${description}">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"/>
+    </a>
+  </li>`
+}).join("");
+
+listEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightboxModal = new SimpleLightbox('.gallery a', 
+{
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
