50c5c78c46cf3123f9d21d5ab36910af6e4d029e
Oksana Jarosch <ok.jarosch@gmail.com>
2023-09-08 22:46:18 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle  from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+
+const LS_KEY = 'videoplayer-current-time';
+
+// Save the time in LS
+
+player.on('timeupdate', throttle(addKeyToLocalStorage, 1000));
+
+function addKeyToLocalStorage(evt) {
+  const currentTime = evt.seconds;
+  localStorage.setItem(LS_KEY, JSON.stringify(currentTime));
+}
+
+// Get the time from LS and setCurrentTime
+
+const savedTime = JSON.parse(localStorage.getItem(LS_KEY));
+console.log(savedTime);
+
+player.setCurrentTime(savedTime);
